40a8b9b8207ca0fcd89a0e436b48bab7844d284f
tonyj0721 <tonyj0721@gmail.com>
2025-11-09 13:29:18 +0800

    Create firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBw9GBw5-LZkKD34qTiwBtZXdwONHLqG_s",
+  authDomain: "straystation.firebaseapp.com",
+  projectId: "straystation",
+  storageBucket: "straystation.firebasestorage.app",
+  messagingSenderId: "",
+  appId: ""
+};
+
+firebase.initializeApp(firebaseConfig);
