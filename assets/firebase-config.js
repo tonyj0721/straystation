9c813db3edf901fb8e78cb3e8c1d049066822002
tonyj0721 <tonyj0721@gmail.com>
2025-12-08 16:15:40 +0800

    Add files via upload

---
+++ assets/firebase-config.js
@@ -0,0 +1,9 @@
+// assets/firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4NT2KOm_Cw2OEdaaOimxI0ruoNv_u_9g",
+  authDomain: "straystation.firebaseapp.com",
+  projectId: "straystation",
+  storageBucket: "straystation.firebasestorage.app",
+  messagingSenderId: "611366379195",
+  appId: "1:611366379195:web:ef5a632e88d8bba1d6139e",
+};
