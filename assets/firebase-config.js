8da68e213b191f5615e69b8877bd8210ad4068b5
tonyj0721 <tonyj0721@gmail.com>
2025-12-13 02:20:53 +0800

    Update firebase-config.js

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -1,9 +1,10 @@
 // assets/firebase-config.js
 window.firebaseConfig = {
-  apiKey: "AIzaSyC4NT2KOm_Cw2OEdaaOimxI0ruoNv_u_9g",
+  apiKey: "AIzaSyCT9U9WETVOFb1rfIkjjjBVteS65Q-Xg1A",
   authDomain: "straystation.firebaseapp.com",
   projectId: "straystation",
   storageBucket: "straystation.firebasestorage.app",
   messagingSenderId: "611366379195",
   appId: "1:611366379195:web:ef5a632e88d8bba1d6139e",
 };
+
