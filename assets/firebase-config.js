1f2a647797239f50ffaf568056d7784d98029b0f
tonyj0721 <tonyj0721@gmail.com>
2025-12-21 12:18:27 +0800

    Add files via upload

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -7,22 +7,3 @@ window.firebaseConfig = {
   messagingSenderId: "611366379195",
   appId: "1:611366379195:web:ef5a632e88d8bba1d6139e",
 };
-
-// 允許登入管理後台的 Google 帳號白名單
-window.firebaseAdminWhitelist = {
-  // 精準 email 白名單（建議填真實帳號）
-  emails: [
-    "tonyj0721@gmail.com",
-    "bbb@gmail.com",
-    "ccc@gmail.com",
-    "ddd@gmail.com",
-    "eee@gmail.com",
-    "fff@gmail.com",
-  ],
-
-  // 如果你有自家網域 (Google Workspace) 也可以直接整個網域開放
-  // 沒有的話就留空陣列就好
-  domains: [
-    // "yourorg.com",
-  ],
-};
