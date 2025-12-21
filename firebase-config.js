e5bb25eed356c49c3ca18bde2ccbd0c2656d51d5
tonyj0721 <tonyj0721@gmail.com>
2025-12-21 12:19:56 +0800

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// assets/firebase-config.js
-window.firebaseConfig = {
-  apiKey: "AIzaSyCT9U9WETVOFb1rfIkjjjBVteS65Q-Xg1A",
-  authDomain: "straystation.firebaseapp.com",
-  projectId: "straystation",
-  storageBucket: "straystation.firebasestorage.app",
-  messagingSenderId: "611366379195",
-  appId: "1:611366379195:web:ef5a632e88d8bba1d6139e",
-};
