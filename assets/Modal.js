91e648caee2e15f607f51a2992bad2c9c868a01e
tonyj0721 <tonyj0721@gmail.com>
2026-02-12 12:55:23 +0800

    Add files via upload

--- assets/Modal.js
+++ assets/Modal.js
@@ -647,7 +647,7 @@ function showWatermarkProgressSwal(opts = {}) {
           <div id="${uid}_fill" style="height:100%; width:0%; border-radius:999px; background:linear-gradient(90deg, #ffd2a6, #d7f2c2); transition:width 120ms linear;"></div>
         </div>
         <img id="${uid}_dog" src="${imgSrc}" alt="" style="position:absolute; top:-18px; left:0; transform:translate(-50%, 0); height:92px; width:auto; pointer-events:none; user-select:none;" />
-        <div id="${uid}_label" style="position:absolute; left:0; right:0; bottom:-6px; text-align:center; font-size:14px; color:#333;">Loading...0%</div>
+        <div id="${uid}_label" style="position:absolute; left:0; right:0; bottom:-3px; text-align:center; font-size:14px; color:#333;">Loading...0%</div>
       </div>
       ${text ? `<div style="margin-top:10px; color:#555; font-size:14px;">${text}</div>` : ""}
     </div>
