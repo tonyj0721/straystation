584df18f84a83de65443685dba6e5d2f39d8aade
tonyj0721 <tonyj0721@gmail.com>
2026-02-13 02:10:30 +0800

    Add files via upload

--- assets/Modal.js
+++ assets/Modal.js
@@ -554,7 +554,7 @@ function startProgressBar(btn, opts = {}) {
   label.style.position = "absolute";
   label.style.left = "0";
   label.style.right = "0";
-  label.style.bottom = "6px";
+  label.style.bottom = "0px";
   label.style.fontSize = "12px";
   label.style.fontWeight = "600";
   label.style.color = "#fff";
@@ -646,7 +646,7 @@ function showWatermarkProgressSwal(opts = {}) {
         <div style="position:absolute; left:0; right:0; bottom:16px; height:22px; border-radius:999px; background:rgba(0,0,0,0.08); overflow:hidden;">
           <div id="${uid}_fill" style="height:100%; width:0%; border-radius:999px; background:linear-gradient(90deg, #ffd2a6, #d7f2c2); transition:width 120ms linear;"></div>
         </div>
-        <img id="${uid}_dog" src="${imgSrc}" alt="" style="position:absolute; bottom:21px;; left:0; transform:translate(-50%, 0); height:92px; width:auto; pointer-events:none; user-select:none;" />
+        <img id="${uid}_dog" src="${imgSrc}" alt="" style="position:absolute; bottom:11px;; left:0; transform:translate(-50%, 0); height:92px; width:auto; pointer-events:none; user-select:none;" />
         <div id="${uid}_label" style="position:absolute; left:0; right:0; bottom:-12px; text-align:center; font-size:14px; color:#333;">Loading...0%</div>
       </div>
       ${text ? `<div style="margin-top:10px; color:#555; font-size:14px;">${text}</div>` : ""}
