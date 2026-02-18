d56ce06efde537cbaa779d3895b4f382d7a8da03
tonyj0721 <tonyj0721@gmail.com>
2026-02-18 13:45:02 +0800

    Add files via upload

--- assets/Modal.js
+++ assets/Modal.js
@@ -482,7 +482,7 @@ function startDots(span, base) {
 // ===============================
 function startProgressBar(btn, opts = {}) {
   const imgSrc = opts.imgSrc || "images/奔跑貓咪.png";
-  const height = opts.height || 64;
+  const height = opts.height || 61;
 
   const original = {
     html: btn.innerHTML,
@@ -544,7 +544,7 @@ function startProgressBar(btn, opts = {}) {
   cat.alt = "";
   cat.decoding = "async";
   cat.style.position = "absolute";
-  cat.style.bottom = "21px"; // 在進度條上方
+  cat.style.bottom = "22px"; // 在進度條上方
   cat.style.left = "0%";
   cat.style.transform = "translateX(-50%)";
   cat.style.height = "68px";
